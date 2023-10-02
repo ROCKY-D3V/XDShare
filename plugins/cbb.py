c41ea4e78ba31b8899be9be3e43db0b4ea5bffa1
ʀσƈƙყ <146221888+ROCKY-D3V@users.noreply.github.com>
2023-10-02 08:55:54 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/ROCKY-D3V/XDShare>Click here</a>\n○ Channel : @XD_NETWORKS\n○ Support Group : @XD_CHATING</b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/ROCKY-D3V/XDShare>Click here</a>\n○ Channel : @XD_SUPORT\n○ Support Group : @XD_CHATING</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
